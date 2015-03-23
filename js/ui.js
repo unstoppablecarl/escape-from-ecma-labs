f4ca6df26b1908784d3424e22c5f21b8f5401517
Carl Olsen <unstoppablecarlolsen@gmail.com>
2015-03-23 10:48:39 -0400

    ui

--- js/ui.js
+++ js/ui.js
@@ -1,34 +1,118 @@
-var $inventoryContainer = document.getElementById('inventory-container');
+// rivets.binders
+(function(root) {
+    'use strict';
 
-var scope = {
-    player: game.player,
-    inventory: game.player.inventory,
-};
+    rivets.binders.color = function(el, color) {
+        if (color) {
+            el.style.color = color;
+        }
+    };
 
-rivets.binders.color = function(el, color) {
-    if(color){
-        el.style.color = color;
-    }
-};
+    rivets.binders.use_item = {
+        bind: function(el) {
+            var adapter = rivets.adapters['.'];
+            var model = this.model;
+            var keypath = this.keypath;
 
-rivets.binders.use_item = {
-    bind: function(el) {
-        var adapter = rivets.adapters['.'];
-        var model = this.model;
-        var keypath = this.keypath;
+            this.callback = function() {
+                var viewItem = adapter.get(model, keypath);
+                var player = model.player;
+                var item = player.inventory.getFirstByType(viewItem.type);
+                player.performAction('use_item', item);
+            };
 
-        this.callback = function() {
-            console.log('click');
-            var item = adapter.get(model, keypath);
-            var player = model.player;
-            player.performAction('use_item', item);
+            $(el).on('click', this.callback);
+        },
+        unbind: function(el) {
+            $(el).off('click', this.callback);
+        }
+    };
+
+    rivets.binders.equip_item = {
+        bind: function(el) {
+            var adapter = rivets.adapters['.'];
+            var model = this.model;
+            var keypath = this.keypath;
+
+            var slot = model.slot;
+
+            this.callback = function() {
+                var viewItem = adapter.get(model, keypath);
+                var player = model.player;
+                var item = player.inventory.getFirstByType(viewItem.type);
+
+                if(player.canEquip(item)){
+                    player.inventory.remove(item);
+                    player.equip(item, slot);
+                }
+            };
+
+            $(el).on('click', this.callback);
+        },
+        unbind: function(el) {
+            $(el).off('click', this.callback);
+        }
+    };
+
+}(this));
+
+(function(root) {
+    'use strict';
+
+    var elById = document.getElementById.bind(document);
+
+    var makeInventory = function(game, elementId){
+        elementId = elementId || 'inventory-container';
+        var el = elById(elementId);
+        var data = {
+            player: game.player,
+            inventoryViewData: game.player.inventory.viewObjects,
         };
+        rivets.bind(el, data);
+    };
 
-        $(el).on('click', this.callback);
-    },
-    unbind: function(el) {
-        $(el).off('click', this.callback);
-    }
-};
+    var makeEquipment = function(game, elementId){
+        elementId = elementId || 'equipment-container';
+        var el = elById(elementId);
+        var data = {
+            player: game.player,
+            slots: game.player.equipment,
+            inventoryViewData: game.player.inventory.viewObjects,
+        };
+        rivets.bind(el, data);
+    };
 
-rivets.bind($inventoryContainer, scope);
+    var makeControls = function(keyBindings, elementId){
+        elementId = elementId || 'controls-container';
+        var el = elById(elementId);
+
+        var bindings = [];
+        for(var action in keyBindings){
+            var keys = keyBindings[action];
+            bindings.push({
+                name: action,
+                keys: keys.join(', ')
+            });
+        }
+        var data = {
+            keyBindings: bindings
+        };
+        rivets.bind(el, data);
+    };
+
+    var makeStats = function(game, elementId){
+        elementId = elementId || 'stats-container';
+        var el = elById(elementId);
+        var data = game.player;
+        rivets.bind(el, data);
+    };
+
+    root.RL.Views = {
+        inventory: makeInventory,
+        equipment: makeEquipment,
+        controls: makeControls,
+        stats: makeStats,
+    };
+
+
+}(this));
